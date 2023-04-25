9ed8298d9b20f20c493e4d3394a674da906f9516
Richard Sentino <richard@mindginative.com>
2023-04-26 08:29:32 +1200

    initial files

---
+++ README.md
@@ -0,0 +1,12 @@
+# NFT ERC721 Solidity
+
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,22 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+
+async function main() {
+  const RixToken = await ethers.getContractFactory("NFT");
+  console.log('Deploying RixToken ERC721 token...');
+  const token = await RixToken.deploy('RixToken','Rx');
+
+  await token.deployed();
+
+  console.log("RixToken deployed to:", token.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
