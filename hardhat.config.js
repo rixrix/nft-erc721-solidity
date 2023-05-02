6389314b50d1bf51c126f038bd2ffb09f6d302d7
Richard Sentino <richard@mindginative.com>
2023-05-02 19:38:55 +1200

    chore: update hardhat config for testnet deploymen

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
+const { ALCHEMY_API_URL, METAMASK_PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: ALCHEMY_API_URL,
+      accounts: [`0x${METAMASK_PRIVATE_KEY}`],
+    },
+  },
 };
